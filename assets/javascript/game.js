cef24ce880394f00db890e82c5613372de253521
Amy Merrill <amous@Amys-MacBook-Pro.local>
2019-03-05 23:15:42 -0700

    in progress

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+//doc ready/reset
+
+
+
+// create a random number array that generates a number"target-value" between 19 and 120
+
+
+
+// create random array 1 thru 12 that randomly sets separate values to 4 different buttons/images
+
+//create a loop stop when the number values are assigned. 
+
+// create click event that same number per button, writes to document
+
+// create function that adds button number value to "guess-total"(counter)
+
+// write if statement that if "target-number" is equal to "guess-total", player wins: alert 
+// 'you win' and adds 1 to "win-count"
+
+// if "guess-total" is greater than "target-number", player loses, alert "you lose", add 1 to "loss-count"
+
+// if neither conditions are met, function continues running,
+
+//when player wins or loses, game resets and loop runs again.
+//users score and score counter will be set to zero
+
+// place codes inside div containers ex;  value total, target value,  alerts, win/loss totals
